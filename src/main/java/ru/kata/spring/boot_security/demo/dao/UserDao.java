c099fb6fd5b3b2e78ed4a7a6c631452546d173d5
Ilya <127244460+imaslow@users.noreply.github.com>
2023-04-13 07:39:52 +0300

    Add files via upload

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -0,0 +1,20 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.List;
+import java.util.Set;
+
+public interface UserDao {
+    List<User> getUsers();
+    void createUser(User user);
+    void updateUser(User updateUser);
+    User readUser(long id);
+    User deleteUser(long id);
+    User findUserByName(String name);
+    User findByEmail(String username);
+    User findById(long id);
+
+}
+
