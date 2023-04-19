a2c5ee6cb5e7ae2055c859ce0ae649a9e88d0a94
Ilya <127244460+imaslow@users.noreply.github.com>
2023-04-19 15:47:10 +0300

    Delete src directory

--- src/main/resources/static/redirectingToStart.js
+++
@@ -1,7 +0,0 @@
-function redirectingToStart() {
-    let url = 'http://localhost:8080/api/users'
-    fetch(url)
-        .then(response => response.json())
-        .then(data => fillingAll(data))
-        .catch(error => console.log(error))
-}
